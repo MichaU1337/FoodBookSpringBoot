dc58eab4991d41e61d21d6d167f393cdca9c0c9c
MichaU1337 <poczta.michau@gmail.com>
2021-07-06 02:00:57 +0200

    Added mail token confirmation. Current setup uses JavaMailSender and works on test mail server MailDev.

---
+++ src/main/java/com/mmierzwa/foodbook/appuser/AppUserRole.java
@@ -0,0 +1,6 @@
+package com.mmierzwa.foodbook.appuser;
+
+public enum AppUserRole {
+    USER,
+    ADMIN
+}
---
+++ src/main/java/com/mmierzwa/foodbook/registration/RegistrationRequest.java
@@ -0,0 +1,13 @@
+package com.mmierzwa.foodbook.registration;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class RegistrationRequest {
+    private final String firstName;
+    private final String lastName;
+    private final String password;
+    private final String email;
+}
